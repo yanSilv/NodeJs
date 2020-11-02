369420be35bf106e3be5ae6e308fd9a4ed896bda
yanSilva <yansiba84@gmail.com>
2020-11-02 00:30:51 -0300

    Mostrandos os usuarios do banco NeDB

--- restfull/routes/users.js
+++ restfull/routes/users.js
@@ -5,14 +5,20 @@ let db = new NeDB({
 });
 module.exports = function(app){
     app.get('/users', (req, res)=>{
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users:[{
-                name:'HCode!!',
-                email:'hcode@gmail.com',
-                id:1
-            }]
+
+        db.find({}).sort({name:1}).exec((err, users)=>{
+            if (err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({
+                    users
+                });
+            }
         });
     });
 

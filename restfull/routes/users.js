876a4f2c852265243f100ca766b62ec196648907
yanSilva <yansiba84@gmail.com>
2020-11-02 00:16:56 -0300

    Utilizando Banco de dados NeDB

--- restfull/routes/users.js
+++ restfull/routes/users.js
@@ -1,3 +1,8 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename:'users.db',
+    autoload:true
+});
 module.exports = function(app){
     app.get('/users', (req, res)=>{
         res.statusCode = 200;
@@ -12,6 +17,15 @@ module.exports = function(app){
     });
 
     app.post('/users', (req, res)=>{
-        res.json(req.body);
+        db.insert(req.body, (err, user)=>{
+            if(err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            } else {
+                res.status(200).json(user);
+            }
+        });
     });
 };

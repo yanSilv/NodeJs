3c245e1509162f165fa47c5f977313a4920170b8
yanSilva <yansiba84@gmail.com>
2020-11-01 23:18:51 -0300

    Utilizando o 'express' no servidor

--- restfull/index.js
+++ restfull/index.js
@@ -1,31 +1,24 @@
-const http = require('http');
+const express = require('express');
+let app = express();
 
-let server = http.createServer((req, res)=>{
-    console.log('URL: '+req.url);
-    console.log('METHOD: '+req.method);
-
-    switch (req.url) {
-        case '/':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<h1>Olá Mundo!!</h1>');
-        break;
-        case '/users':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-                users:[{
-                    name:'HCode',
-                    email:'hcode@gmail.com',
-                    id:1
-                }]
-            }));
-        break; 
-    }
-
-    res.end('OK');
+app.get('/', (req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Olá Mundo!!</h1>');
 });
 
-server.listen(3000, '127.0.0.1', ()=>{
+app.get('/users', (req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users:[{
+            name:'HCode',
+            email:'hcode@gmail.com',
+            id:1
+        }]
+    });
+});
+
+app.listen(3000, '127.0.0.1', ()=>{
     console.log('servidor rodando!!');
 });
